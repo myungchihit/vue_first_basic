2fdb26f2a944705df5d3149a295e08d2a9cb2a0f
JJangGyu <pridom112@naver.com>
2022-06-05 09:47:07 +0900

    vue-cli basic

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <app-header 
+        v-on:renew="renewStr"
+        v-bind:propsdata="str"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: function () {
+    return {
+      str: 'Header!!'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "Hi!";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
